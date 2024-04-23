5963a91eb146caa862f484263df5687fa697fcbb
Mo David <modavid.1964@gmail.com>
2024-04-23 15:45:25 +0800

    Added cell dimensions.

--- src/dataviser/main.js
+++ src/dataviser/main.js
@@ -1,7 +1,7 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-04-23 08:45:34
- * @ Modified time: 2024-04-23 12:18:24
+ * @ Modified time: 2024-04-23 15:43:16
  * @ Description:
  * 
  * Manages all the dataviser functionality.
@@ -22,28 +22,16 @@ export const dataviser = (function() {
     
     root.innerHTML = `
       <div class='dataviser-window'>
-        <div class='dataviser-title'>
+        <div class='grid-cell grid-1-1 grid-cell-w2 grid-cell-h1'>
           Dataviser
-        </div>
-
-        <div class='grid-cell grid-1-1 grid-cell-w1 grid-cell-h1'>
-          the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.
-        </div>
-
-        <div class='grid-cell grid-2-1 grid-cell-w1 grid-cell-h1'>
-          the quick brown fox jumped over the lazy dog. the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.
         </div>
 
         <div class='grid-cell grid-3-1 grid-cell-w1 grid-cell-h1'>
           the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. 
         </div>
 
-        <div class='grid-cell grid-4-1 grid-cell-w1 grid-cell-h1'>
+        <div class='grid-cell grid-4-1 grid-cell-w1 grid-cell-h2'>
           the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. 
-        </div>
-
-        <div class='grid-cell grid-4-2 grid-cell-w1 grid-cell-h1'>
-          the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. 
         </div>
         
         <div class='grid-cell grid-4-3 grid-cell-w1 grid-cell-h1'>
