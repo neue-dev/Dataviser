76b0fa6da4d402ba22bfb8f06b2008414575df5d
Mo David <modavid.1964@gmail.com>
2024-04-23 17:14:58 +0800

    Created the grid component class and its cells.
    This allows us to create and modify the grid layout purely through JS.
    It also provides another cleaner layer of abstraction.

--- src/dataviser/main.js
+++ src/dataviser/main.js
@@ -1,48 +1,61 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-04-23 08:45:34
- * @ Modified time: 2024-04-23 15:43:16
+ * @ Modified time: 2024-04-23 17:13:38
  * @ Description:
  * 
  * Manages all the dataviser functionality.
  */
 
 import './main.css'
+import '../ui/Grid.component'
+import '../ui/Button.component'
 import '../ui/Slider.component'
+import { ipcRenderer } from 'electron';
 
 // Handles all the data vis
 export const dataviser = (function() {
   const _ = {};
   const root = document.getElementsByClassName('root')[0];
 
+  // Dataviser menu elements
+  const dataviser_window = document.createElement('grid-component');
+
   /**
    * Update this so it doesnt become messy over time
    */
   _.init = function() {
     
-    root.innerHTML = `
-      <div class='dataviser-window'>
-        <div class='grid-cell grid-1-1 grid-cell-w2 grid-cell-h1'>
-          Dataviser
-        </div>
+    const title_cell = document.createElement('grid-cell-component');
+    const title_node = document.createElement('div');
+    const import_cell = document.createElement('grid-cell-component');
+    const import_button = document.createElement('button-component');
 
-        <div class='grid-cell grid-3-1 grid-cell-w1 grid-cell-h1'>
-          the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. 
-        </div>
+    // Create the title
+    title_node.classList.add('dataviser-title');
+    title_node.innerHTML = 'Dataviser';
+    title_cell.setPlacement(1, 1);
+    title_cell.appendChild(title_node);
 
-        <div class='grid-cell grid-4-1 grid-cell-w1 grid-cell-h2'>
-          the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. 
-        </div>
+    // Create the import button and its prompt
+    import_button.innerHTML = 'select folder';
+    import_button.classList.add('dataviser-import-button');
+    import_button.mouseDownCallback = e => {
         
-        <div class='grid-cell grid-4-3 grid-cell-w1 grid-cell-h1'>
-          the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.  the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. 
-        </div>
+      // Let the user pick a directory
+      showDirectoryPicker({ id: 'default' })
+        .then(folder => { console.log(folder.entries()) })
+        .catch(console.log('No or bad directory picked.'))
+    }
 
-        <div class='grid-cell grid-3-3 grid-cell-w1 grid-cell-h1'>
-          the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes.the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. the quick brown fox jumped over the lazy dog lorem ipsum doloret yes. 
-        </div>
-      </div>
-    `;
+    import_cell.setPlacement(1, 2);
+    import_cell.innerHTML = 'Select folder to begin.';
+    import_cell.appendChild(import_button);
+
+    // Construct the tree
+    dataviser_window.appendChild(title_cell);
+    dataviser_window.appendChild(import_cell);
+    root.appendChild(dataviser_window);
   }
 
   return {
---
+++ src/ui/Grid.component.js
@@ -0,0 +1,102 @@
+/**
+ * @ Author: Mo David
+ * @ Create Time: 2024-04-23 16:21:09
+ * @ Modified time: 2024-04-23 16:57:26
+ * @ Description:
+ * 
+ * The grid class.
+ */
+
+import './Grid.component.css'
+import { Component } from './Component'
+
+export class GridComponent extends Component {
+
+  constructor() {
+    super();
+  }
+
+  init() {
+    super.init();
+
+    this.classList.add('grid');
+  }
+}
+
+export class GridCellComponent extends Component {
+  static observedAttributes = [ 'cell-x', 'cell-y', 'cell-w', 'cell-h' ];
+
+  x = 0;
+  y = 0;
+  w = 1;
+  h = 1;
+
+  constructor() {
+    super();
+  }
+
+  /**
+   * Initializes the grid cell.
+   */
+  init() {
+    super.init();
+
+    this.classList.add('grid-cell');
+    this.updateStyle();
+  }
+
+  /**
+   * Updates the parameters of the grid cell.
+   */
+  attributeChangedCallback(name, oldValue, newValue) {
+    if(name == 'cell-x') this.x = parseInt(newValue);
+    if(name == 'cell-y') this.y = parseInt(newValue);
+    if(name == 'cell-w') this.w = parseInt(newValue);
+    if(name == 'cell-h') this.h = parseInt(newValue);
+
+    this.updateStyle();
+  }
+
+  /**
+   * Sets the placement of the cell on the grid.
+   * 
+   * @param   { number }  x   The x location on the grid. 
+   * @param   { number }  y   The y location on the grid.
+   */
+  setPlacement(x, y) {
+    this.setAttribute('cell-x', x);
+    this.setAttribute('cell-y', y);    
+  }
+
+  /**
+   * Sets the dimensions of the cell on the grid.
+   * 
+   * @param   { number }  w   The number of unit cells wide. 
+   * @param   { number }  h   The number of unit cells high.
+   */
+  setDimensions(w, h) {
+    this.setAttribute('cell-w', w);
+    this.setAttribute('cell-h', h);
+  }
+
+  /**
+   * Updates the style of the grid cell based on its attributes.
+   */
+  updateStyle() {
+    this.classList.remove(...this.classList);
+    this.classList.add('grid-cell');
+    this.classList.add('grid-cell-x' + this.x);
+    this.classList.add('grid-cell-y' + this.y);
+    this.classList.add('grid-cell-w' + this.w);
+    this.classList.add('grid-cell-h' + this.h);
+  }
+}
+
+// Register the custom elements
+customElements.define('grid-component', GridComponent);
+customElements.define('grid-cell-component', GridCellComponent);
+
+export default {
+  GridComponent,
+  GridCellComponent,
+}
