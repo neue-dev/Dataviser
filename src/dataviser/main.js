4a0d7b68d79fa56680c088e4d4fc62ee6685f4a2
Mo David <modavid.1964@gmail.com>
2024-04-24 18:41:49 +0800

    Refactored the code and made sure Dataset class did most of the rendering.
    Also, I created two callback functions which would convert our raw
    data into data we can present. The second callback function actually
    helps minimize the lag by accumulating the smaller of the data samples
    so we don't have to render a lot.

--- src/dataviser/main.js
+++ src/dataviser/main.js
@@ -1,7 +1,7 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-04-23 08:45:34
- * @ Modified time: 2024-04-24 15:59:59
+ * @ Modified time: 2024-04-24 18:40:24
  * @ Description:
  * 
  * Manages all the dataviser functionality.
@@ -13,14 +13,14 @@ import '../ui/Grid.component'
 import '../ui/Button.component'
 import '../ui/Slider.component'
 
+import { Dataset } from './Dataset.class'
 import d3 from '../libs/d3.v7.min'
 
 // Handles all the data vis
 export const dataviser = (function() {
   const _ = {};
   const root = document.getElementsByClassName('root')[0];
-  const data = {};
-  const d3Assets = {};
+  const dataset = new Dataset();
 
   // Dataviser menu elements
   const dataviserWindow = document.createElement('grid-component');
@@ -63,6 +63,7 @@ export const dataviser = (function() {
 
     // Create the canvas cell
     canvasCell.setPlacement(3, 1);
+    canvasCell.setDimensions(3, 1);
     canvasCell.appendChild(dataviserCanvas);
 
     // Construct the tree
@@ -73,25 +74,91 @@ export const dataviser = (function() {
   }
 
   /**
-   * Loads the existing data to be represented on the UI.
+   * Configures the dataset object.
    */
-  _.loadData = function() {
-    // Performs cleanup on the data
-    
-    
-    // for(let dataKey in data) {
-    //   let dataSet = data[dataKey];
-    //   let dataArray = [];
+  _.configData = function() {
 
-    //   // Convert into an array of objects
-    //   for(let dataEntry in dataSet) {
-    //     dataSet[dataEntry]['index'] = dataEntry;
-    //     dataArray.push(dataSet[dataEntry]);
-    //   }
+    // This parser converts the raw data into a 2x2 matrix
+    dataset.addParser('matrix', asset => {
 
-    //   // Convert intro a dataframe
-    //   jd.dfFromObjArray(dataArray).p()
-    // }
+      // We define a matrix
+      let m = [];
+      m.labels = {};
+        
+      // Build up the matrix
+      for(let row in asset) {
+        let mrow = [];
+        
+        for(let entry in asset[row])
+          mrow.push(asset[row][entry]);
+
+        // Push the new row and the label
+        m.push(mrow);
+        m.labels.push(row);
+      }
+
+      return m;
+    });
+
+    // This parser converts the raw data into a 2x2 matrix
+    dataset.addParser('matrix-reduced', asset => {
+
+      // We define a matrix
+      let m = [];
+      let sums = [];
+      m.labels = [];
+        
+      // Generate sums per row
+      for(let row in asset) {
+        let sum = 0;
+        
+        for(let entry in asset[row])
+          sum += asset[row][entry];
+        sums.push([row, sum]);
+      }
+
+      // Sort sums by size
+      sums.sort((a, b) => b[1] - a[1]);
+
+      // Get only the 20 most prominent locations
+      let i = 0;
+      
+      for(; i < 2 && i < sums.length; i++) {
+        let mrow = [];
+        let row = sums[i][0];
+
+        for(let entry in asset[row])
+          mrow.push(asset[row][entry]);
+
+        // Push the new row and the label
+        m.push(mrow);
+        m.labels.push(row);
+      }
+
+
+      // The last matrix row
+      let mlastrow = [];
+
+      for(; i < sums.length; i++) {
+        let row = sums[i][0];
+        let j = 0;
+
+        // Accumulate the remaining values
+        for(let entry in asset[row]) {
+          if(j >= mlastrow.length)
+            mlastrow[j] = 0;
+          mlastrow[j++] += asset[row][entry];
+        }
+      }
+
+      // If it's not empty
+      if(mlastrow.length) {
+        m.push(mlastrow);
+        m.labels.push('others');
+      }
+
+      return m;
+    });
   }
 
   /**
@@ -99,109 +166,9 @@ export const dataviser = (function() {
    */
   _.renderData = function() {
 
-    // The matrices we're gonna create
-    const matrices = {};
-    const names = [];
-    const colors = [];
-
-    // The actual graphs and stuff
-    d3Assets.chords = {};
-    d3Assets.ribbons = {};
-
     // For each data set, we create a matrix
-    for(let dataSetKey in data) {
-      let matrix = [];
-      
-      for(let dataRow in data[dataSetKey]) {
-        let matrixRow = [];
-        let count = 0, c = '';
-        
-        for(let dataEntry in data[dataSetKey][dataRow]) { 
-          matrixRow.push(data[dataSetKey][dataRow][dataEntry]);
-          names[count] = dataEntry;
-          
-          c = (count * 3).toString(16).length < 2 ? '0' + count.toString(16) : count.toString(16);
-          colors.push(`#${c}3232`);
-          count++;
-          console.log(count);
-        }
-
-        matrix.push(matrixRow);
-      }
-
-      matrices[dataSetKey] = matrix;
-      d3Assets.chords[dataSetKey] = d3
-        .chord()
-        .padAngle(0.01)
-        .sortSubgroups(d3.descending)
-        (matrix);
-
-      // Hover tooltip
-      const tooltip = d3.select(".canvas")
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "1px")
-        .style("border-radius", "5px")
-        .style("padding", "10px")
-        .style("display", "fixed")
-
-      const showTooltip = function(e, d) {
-        tooltip
-          .style("opacity", 1)
-          .style("left", (e.screenX + 15) + "px")
-          .style("top", (e.screenY - 28) + "px")
-          .html("From: " + names[d.source.index] + " To: " + names[d.target.index])
-      }
-
-      const hideTooltip = function(e, d) {
-        tooltip
-          .transition()
-          .duration(1000)
-          .style("opacity", 0)
-      }
-      
-      // Create the svg area first
-      const svg = d3
-        .select(".canvas")
-        .append("svg")
-          .attr("width", 640)
-          .attr("height", 640)
-        .append("g")
-          .attr("transform", "translate(320,320)")
-          
-      svg
-        .datum(d3Assets.chords[dataSetKey])
-        .append("g")
-        .selectAll("path")
-        .data(function(d) { return d; })
-        .enter()
-        .append("path")
-          .attr("d", d3.ribbon()
-            .radius(300)
-          )
-          .style("fill", function(d){ return colors[d.source.index] })
-          .style("stroke", "transparent")
-          .on("mouseover", showTooltip )
-          .on("mouseleave", hideTooltip )
-          
-      svg
-        .datum(d3Assets.chords[dataSetKey])
-        .append("g")
-        .selectAll("g")
-        .data(function(d) { return d.groups; })
-        .enter()
-        .append("g")
-        .append("path")
-          .style("fill", function(d,i){ return colors[i] })          
-          .style("stroke", "transparent")
-          .attr("d", d3.arc()
-            .innerRadius(310)
-            .outerRadius(320)
-          )
-
+    for(let dataSetKey in dataset.assets) {
+      dataset.renderChord(dataSetKey, { assetParserKey: 'matrix-reduced' });
       return;
     }
   }
@@ -231,21 +198,12 @@ export const dataviser = (function() {
           for await(let entryHandle of folderHandle.values()) {
           
             // Add subdirectory to queue
-            if(entryHandle instanceof FileSystemDirectoryHandle) {
+            if(entryHandle instanceof FileSystemDirectoryHandle)
               folderHandles.push(entryHandle);
 
             // Add file to file list
-            } else {
-              let reader = new FileReader();
-              let file = await entryHandle.getFile();
-              let key = file.name.split('.').slice(0, -1).join('.');
-
-              // Parse the content of the file
-              reader.readAsText(file);
-              reader.onload = e => {
-                data[key] = JSON.parse(e.target.result);
-              }
-            }
+            else
+              dataset.readJSON(await entryHandle.getFile());
           }
 
         // While we have stuff in the queue
@@ -256,7 +214,7 @@ export const dataviser = (function() {
 
       // Load data
       }).then(() => {
-        _.loadData();
+        _.configData();
         _.renderData();
       })
 
