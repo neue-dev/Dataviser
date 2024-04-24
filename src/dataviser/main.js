02e2a96c0c72894e8f3f4c397d951f195a73282e
Mo David <modavid.1964@gmail.com>
2024-04-24 12:29:03 +0800

    Implemented file reading.
    JSON files can now be parsed by the client side and are stored in
    memory as is.

--- src/dataviser/main.js
+++ src/dataviser/main.js
@@ -1,7 +1,7 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-04-23 08:45:34
- * @ Modified time: 2024-04-24 12:02:48
+ * @ Modified time: 2024-04-24 12:28:57
  * @ Description:
  * 
  * Manages all the dataviser functionality.
@@ -19,8 +19,7 @@ import d3 from '../libs/d3.v7.min'
 export const dataviser = (function() {
   const _ = {};
   const root = document.getElementsByClassName('root')[0];
-  const reader = new FileReader();
-  const files = {};
+  const data = {};
 
   // Dataviser menu elements
   const dataviserWindow = document.createElement('grid-component');
@@ -61,6 +60,7 @@ export const dataviser = (function() {
 
   /**
    * Selects a directory for the user.
+   * This function reads all the JSON files within a directory and stores them as is within our JS object.
    */
   _.selectDirectory = function() {
     
@@ -69,21 +69,39 @@ export const dataviser = (function() {
 
       // After selecting a folder
       .then(async folderHandle => {
-      
-        // Go through each of the files in the directory and save them to file table
-        for await(let fileHandle of folderHandle.values()) {
 
-          // Grab the file and the filename (without extension)
-          const file = await fileHandle.getFile();
-          const key = file.name.split('.').slice(0, -1).join('.');
+        // Queue of the different directories to parse
+        let folderHandles = [ folderHandle ];
+        let i = 0;
 
-          // Read the text file and send the data to the api when done
-          reader.readAsText(file)
-          reader.onload = e => {
-            console.log(e.target.result);
-            files[key] = e.target.result;
+        do {
+
+          // Go to the next handle
+          folderHandle = folderHandles[i++];
+
+          // For each thing inside the folder
+          for await(let entryHandle of folderHandle.values()) {
+          
+            // Add subdirectory to queue
+            if(entryHandle instanceof FileSystemDirectoryHandle) {
+              folderHandles.push(entryHandle);
+
+            // Add file to file list
+            } else {
+              let reader = new FileReader();
+              let file = await entryHandle.getFile();
+              let key = file.name.split('.').slice(0, -1).join('.');
+
+              // Parse the content of the file
+              reader.readAsText(file);
+              reader.onload = e => {
+                data[key] = JSON.parse(e.target.result);
+              }
+            }
           }
-        }
+
+        // While we have stuff in the queue
+        } while(i < folderHandles.length);
 
         // Success
         return true;
