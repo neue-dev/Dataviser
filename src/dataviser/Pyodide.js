184c44998a960481a8a84aae989832ad77477635
Mo David <modavid.1964@gmail.com>
2024-04-27 10:44:01 +0800

    Got the webworkers working with pyodide.

--- src/dataviser/Pyodide.js
+++ src/dataviser/Pyodide.js
@@ -1,28 +1,44 @@
+
+// We import the module from a cdn
 if(typeof importScripts == 'function') {
-  self.languagePluginUrl = 'http://localhost:8000/'
   importScripts('https://cdn.jsdelivr.net/pyodide/v0.25.1/full/pyodide.js')
 
-  var onmessage = function(e) { // eslint-disable-line no-unused-vars
-    languagePluginLoader.then(() => {
-      self.pyodide.loadPackage(['numpy', 'pytz']).then(() => {
-        const data = e.data;
-        const keys = Object.keys(data);
-        for (let key of keys) {
-          if (key !== 'python') {
-            // Keys other than python must be arguments for the python script.
-            // Set them on self, so that `from js import key` works.
-            self[key] = data[key];
-          }
-        }
-        self.pyodide.runPythonAsync(data.python, () => {})
-            .then((results) => { self.postMessage({results}); })
-            .catch((err) => {
-              // if you prefer messages with the error
-              self.postMessage({error : err.message});
-              // if you prefer onerror events
-              // setTimeout(() => { throw err; });
-            });
-      });
-    });
+  /**
+   * This function configures pyodide and the packages we need.
+   */
+  async function configPyodide() {
+    
+    // Define pyodide
+    self.pyodide = await loadPyodide();
+
+    // Load the packages we need
+    await self.pyodide.loadPackage([ 'numpy' ]);
+  }
+
+  const configPyodidePromise = configPyodide();
+
+  /**
+   * This is the event handler for messages received from the main thread.
+   * 
+   * @param   { event }   e   The event object. 
+   */
+  self.onmessage = async (e) => {
+    
+    await configPyodidePromise;
+
+    const { id, python, ...context } = e.data;
+
+    for (const key of Object.keys(context)) {
+      self[key] = context[key];
+    }
+
+    // Now is the easy part, the one that is similar to working in the main thread:
+    try {
+      await self.pyodide.loadPackagesFromImports(python);
+      let results = await self.pyodide.runPythonAsync(python);
+      self.postMessage({ results, id });
+    } catch (error) {
+      self.postMessage({ error: error.message, id });
+    }
   }
 }
--- src/dataviser/main.js
+++ src/dataviser/main.js
@@ -1,25 +1,54 @@
+
 const pyodideWorker = new Worker(new URL('./Pyodide.js', import.meta.url))
 
-pyodideWorker.onerror = (e) => {
-  console.log(e);
-  console.log(`Error in pyodideWorker at ${e.filename}, Line: ${e.lineno}, ${e.message}`)
-}
+const callbacks = {};
 
-pyodideWorker.onmessage = (e) => {
-  const {results, error} = e.data
-  if (results) {
-    console.log('pyodideWorker return results: ', results)
-  } else if (error) {
-    console.log('pyodideWorker error: ', error)
+pyodideWorker.onmessage = e => {
+  const { id, ...data } = e.data;
+  const onSuccess = callbacks[id];
+  delete callbacks[id];
+  onSuccess(data);
+};
+
+const asyncRun = (() => {
+  let id = 0; // identify a Promise
+  return (script, context) => {
+    // the id could be generated more carefully
+    id = (id + 1) % Number.MAX_SAFE_INTEGER;
+    return new Promise((onSuccess) => {
+      callbacks[id] = onSuccess;
+      pyodideWorker.postMessage({
+        ...context,
+        python: script,
+        id,
+      });
+    });
+  };
+})();
+
+const script = `
+    import statistics
+    from js import A_rank
+    statistics.stdev(A_rank)
+`;
+
+const context = {
+  A_rank: [0.8, 0.4, 1.2, 3.7, 2.6, 5.8],
+};
+
+async function main() {
+  try {
+    const { results, error } = await asyncRun(script, context);
+    if (results) {
+      console.log("pyodideWorker return results: ", results);
+    } else if (error) {
+      console.log("pyodideWorker error: ", error);
+    }
+  } catch (e) {
+    console.log(
+      `Error in pyodideWorker at ${e.filename}, Line: ${e.lineno}, ${e.message}`,
+    );
   }
 }
 
-const data = {
-  A_rank: [0.8, 0.4, 1.2, 3.7, 2.6, 5.8],
-  python:
-    'import statistics\n' +
-    'from js import A_rank\n' +
-    'statistics.stdev(A_rank)'
-}
-
-pyodideWorker.postMessage(data)
+main();
