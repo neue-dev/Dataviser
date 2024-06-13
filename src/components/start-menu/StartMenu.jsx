b0c76e682942ee3b506c20e50a1dae8a027c5d62
Mo David <modavid.1964@gmail.com>
2024-06-14 01:35:07 +0800

    Finished start menu.

--- src/components/start-menu/StartMenu.jsx
+++ src/components/start-menu/StartMenu.jsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import { useState } from 'react'
+import { useRef } from 'react'
 
 // Chakra
-import { Center } from '@chakra-ui/react'
+import { Center, background } from '@chakra-ui/react'
 
 // Spring
-import { animated, useSpring } from '@react-spring/web'
-
-// Custom
-import { DRevealer } from '../base/DRevealer.jsx';
+import { Controller, animated, useSpring } from '@react-spring/web'
 
 /**
  * The startup component contains the prompt we give to the user.
@@ -17,19 +14,69 @@ import { DRevealer } from '../base/DRevealer.jsx';
  * @component 
  */
 export function StartMenu() {
-  const [ isOpen, setIsOpen ] = useState(true);
 
+  // Component state
+  let isOpen = useRef(false);
+
+  // Some start menu constants
+  const initialWidth = '2vw';
+  const defaultWidth = '20vw';
+
+  // Our animations for the component
+  const animations = {
+    panel: new Controller({ width: defaultWidth, }),
+  };
+
+  /**
+   * Opens the start menu.
+   */
   function open() {
-    console.log('opened');
+    
+    // Prevents glitchy anim
+    if(isOpen)
+      return;
+
+    animations.panel.start({
+      from: { width: initialWidth, },
+      to:   { width: defaultWidth, }
+    });
+
+    isOpen = true;
   }
 
+  /**
+   * Closes the start menu.
+   */
   function close() {
-    console.log('closed');
+
+    // Prevents glitchy anim
+    if(!isOpen)
+      return;
+
+    animations.panel.start({
+      from: { width: defaultWidth, },
+      to:   { width: initialWidth, }
+    });
+
+    isOpen = false;
   }
 
+  /**
+   * The component JSX.
+   */
   return (
-    <animated.div>
-      <DRevealer onHover={ open } onLeave={ close }/>
+    <animated.div
+      
+      // Styling
+      style={{
+        height: '100vh',
+        background: '#81E6D9',
+        ...animations.panel.springs,
+      }}
+
+      // Callback
+      onMouseLeave={ close }
+      onMouseEnter={ open }>
     </animated.div>
   )
 }
