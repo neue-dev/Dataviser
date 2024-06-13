1f503eb14856f75809de3a6433ea8eefb9ec2e54
Mo David <modavid.1964@gmail.com>
2024-06-14 02:22:11 +0800

    Minor updates.

--- src/components/start-menu/StartMenu.jsx
+++ src/components/start-menu/StartMenu.jsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { useRef } from 'react'
+import { useRef } from 'react';
+import { useState } from 'react';
 
 // Chakra
-import { Center, background } from '@chakra-ui/react'
+import { Container, Center, Box, Heading, Button, Stack, Text } from '@chakra-ui/react'
 
 // Spring
 import { Controller, animated, useSpring } from '@react-spring/web'
+
+// APIs
+import { ClientFS } from '../../client/client.fs'
 
 /**
  * The startup component contains the prompt we give to the user.
@@ -19,12 +23,12 @@ export function StartMenu() {
   let isOpen = useRef(false);
 
   // Some start menu constants
-  const initialWidth = '2vw';
-  const defaultWidth = '20vw';
+  const initialX = '-12rem';
+  const defaultX = '0';
 
   // Our animations for the component
   const animations = {
-    panel: new Controller({ width: defaultWidth, }),
+    panel: new Controller({ x: defaultX, }),
   };
 
   /**
@@ -37,8 +41,8 @@ export function StartMenu() {
       return;
 
     animations.panel.start({
-      from: { width: initialWidth, },
-      to:   { width: defaultWidth, }
+      from: { x: initialX, },
+      to:   { x: defaultX, }
     });
 
     isOpen = true;
@@ -54,8 +58,8 @@ export function StartMenu() {
       return;
 
     animations.panel.start({
-      from: { width: defaultWidth, },
-      to:   { width: initialWidth, }
+      from: { x: defaultX, },
+      to:   { x: initialX, }
     });
 
     isOpen = false;
@@ -69,14 +73,104 @@ export function StartMenu() {
       
       // Styling
       style={{
+        width: '14rem',
         height: '100vh',
-        background: '#81E6D9',
+        background: '#CCCCCC',
         ...animations.panel.springs,
       }}
 
       // Callback
       onMouseLeave={ close }
       onMouseEnter={ open }>
+
+      <_StartMenuTitle />
+      <_StartMenuButton />
+      <_StartMenuFiles />
     </animated.div>
+  )
+}
+
+/**
+ * The start menu title
+ * 
+ * @component
+ */
+const _StartMenuTitle = function() {
+  return (
+    <Box
+      px="1.6rem"
+      py="3.2rem">
+      <Heading
+        fontSize="2.5rem">
+        dataviser
+      </Heading>
+    </Box>
+  )
+}
+
+/**
+ * The start menu description
+ * 
+ * @component 
+ */
+const _StartMenuButton = function() {
+  return (
+    <Box px="1.6rem" py="0" my="0">
+      <Button 
+        onClick={ ClientFS.chooseDirectories }>
+        open folder
+      </Button>
+    </Box>
+  )
+}
+
+/**
+ * Displays the files loaded by the start menu.
+ * 
+ * @component 
+ */
+const _StartMenuFiles = function() {
+
+  // Stores the files contained by the table
+  const [ files, setFiles ] = useState([]);
+  let i = 0;
+
+  // ! to a better solution instead of listening for a message thats not intended for this component
+  window.addEventListener('message', e => {
+    if(e.data.target == 'fs') {
+      setFiles(ClientFS.getRefList());
+      i = 0;
+    }
+  });
+
+  // If no files, don't show the table
+  if(!files.length)
+    return (<></>);
+
+  return (
+    <Container 
+      px="3.6rem" py="0" my="2rem" mx="-2rem"
+      overflowX="hidden" overflowY="auto" 
+      maxHeight="60vh">
+      
+      <Stack> { 
+        files.map(file => {
+
+          // Parse the file entry
+          // ! fix the filename parsing line; it looks bad
+          const id = file.id;
+          const filename = file.filepath.split('\\')[file.filepath.split('\\').length - 1];
+
+          // Create the file entry component
+          return (
+            <Box key={ id }>
+              <Text fontSize="0.5em" fontWeight="400">
+                { `${filename}\t(${++i})` }
+              </Text>
+            </Box>
+          )
+        }) 
+      } </Stack>
+    </Container>
   )
 }
