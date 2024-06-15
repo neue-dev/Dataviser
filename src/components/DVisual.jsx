66cef5f4464f99de474daa2820cc0dfef957c07b
Mo David <modavid.1964@gmail.com>
2024-06-15 23:37:02 +0800

    Got d3 working with react; created a temp bar chart.

--- src/components/DVisual.jsx
+++ src/components/DVisual.jsx
@@ -1,69 +1,109 @@
 
 
 import * as React from 'react'
-import { useRef } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 import * as d3 from 'd3'
 
-export function DVisual() {
+// Chakra
+import { Box } from '@chakra-ui/react'
 
-  // set the dimensions and margins of the graph
-  var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-  width = 460 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+/**
+ * The DVisual component houses a D3-backed component.
+ * 
+ * @component 
+ */
+export function DVisual(props={}) {
+  return (
+    <Box>
+      <_DVisualD3 />
+      hey?
+    </Box>
+  )
+}
 
-  // append the svg object to the body of the page
-  var svg = d3
-    .select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+/**
+ * This manages the state of the D3 aspect of the component.
+ * 
+ * @component 
+ */
+function _DVisualD3(props={}) {
+  const [ data, setData ] = useState([
 
-  // Parse the Data
-  d3.csv(
-    "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv",
-    function (data) {
-      // // X axis
-      // var x = d3
-      //   .scaleBand()
-      //   .range([0, width])
-      //   .domain(
-      //     data.map(function (d) {
-      //       return d.Country;
-      //     })
-      //   )
-      //   .padding(0.2);
-      // svg
-      //   .append("g")
-      //   .attr("transform", "translate(0," + height + ")")
-      //   .call(d3.axisBottom(x))
-      //   .selectAll("text")
-      //   .attr("transform", "translate(-10,0)rotate(-45)")
-      //   .style("text-anchor", "end");
+    {
+      name: "A",
+      value: 50,
+    },
+    {
+      name: "B",
+      value: 20,
+    },
+    {
+      name: "C",
+      value: 40,
+    },
+    {
+      name: "D",
+      value: 70,
+    },
+  ]);
 
-      // // Add Y axis
-      // var y = d3.scaleLinear().domain([0, 13000]).range([height, 0]);
-      // svg.append("g").call(d3.axisLeft(y));
+  useEffect(() => {
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
 
-      // // Bars
-      // svg
-      //   .selectAll("mybar")
-      //   .data(data)
-      //   .enter()
-      //   .append("rect")
-      //   .attr("x", function (d) {
-      //     return x(d.Country);
-      //   })
-      //   .attr("y", function (d) {
-      //     return y(d.Value);
-      //   })
-      //   .attr("width", x.bandwidth())
-      //   .attr("height", function (d) {
-      //     return height - y(d.Value);
-      //   })
-      //   .attr("fill", "#69b3a2");
-    }
-  );
+    const x = d3.scaleBand().range([0, width]).padding(0.1);
+    const y = d3.scaleLinear().range([height, 0]);
+
+    const svg = d3
+      .select(".dvisual")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(
+      data.map(function (d) {
+        return d.name;
+      })
+    );
+    y.domain([
+      0,
+      d3.max(data, function (d) {
+        return d.value;
+      }),
+    ]);
+
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function (d) {
+        return x(d.name);
+      })
+      .attr("width", x.bandwidth())
+      .attr("y", function (d) {
+        return y(d.value);
+      })
+      .attr("height", function (d) {
+        return height - y(d.value);
+      });
+
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    svg.append("g").call(d3.axisLeft(y));
+  }, [data])
+
+  return (
+    <div className='dvisual'>
+    </div>
+  )
 }
