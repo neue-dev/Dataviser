946a35fd72ae9b9b20954acd286cbc5b806fb2c0
Mo David <modavid.1964@gmail.com>
2024-06-16 01:08:18 +0800

    Created file for storing app-specific Py scripts.

---
+++ src/components/Dataviser.py.js
@@ -0,0 +1,18 @@
+/**
+ * @ Author: Mo David
+ * @ Create Time: 2024-06-16 01:07:02
+ * @ Modified time: 2024-06-16 01:08:01
+ * @ Description:
+ * 
+ * This contains all the app-specific scripts that we might need.
+ */
+
+const DataviserPy = (function() {
+  
+  const _ = {};
+
+  return {
+    ..._,
+  }
+})();
+
