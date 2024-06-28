eed7bf22bd7310c1bc83222710825262e16bf0d7
Mo David <modavid.1964@gmail.com>
2024-06-28 21:42:41 +0800

    Added slice for storing the dataframes.

---
+++ src/data/df.slice.js
@@ -0,0 +1,106 @@
+/**
+ * @ Author: Mo David
+ * @ Create Time: 2024-06-28 21:12:24
+ * @ Modified time: 2024-06-28 21:41:45
+ * @ Description:
+ * 
+ * This portion of redux manages our dataframe data.
+ */
+
+// Redux
+import { createSlice } from "@reduxjs/toolkit";
+
+// Our dataframe slice
+export const dfSlice = createSlice({
+  
+  // Name of the slice
+  name: 'df',
+
+  // Initial state
+  initialState: {
+
+    // The loading state of the df slice
+    state: 'idle',
+
+    // Store all our dataframe data, indexed by unique id
+    dfs: {},
+  },
+
+  // Reducers
+  reducers: {
+    
+    /**
+     * Adds a dataframe to the slice.
+     * Uses the provided id to identify the dataframe.
+     * 
+     * @param { state }   state   The value of the current state. 
+     * @param { action }  action  The details of the action.
+     */
+    dfCreate: (state, action) => {
+
+      // Get the action details
+      const id = action?.payload?.id ?? null;
+      const data = action?.payload?.data ?? null;
+
+      // We don't generate ids within this method
+      // They must be provided, otherwise nothing happens
+      // Also, we can't save nullish data
+      if(id == null || data == null)
+        return;
+
+      // Save the data
+      state.dfs[id] = data;
+    },
+
+    /**
+     * Remove a dataframe from the slice.
+     * Uses the id of the dataframe to identify it.
+     * 
+     * @param { state }   state   The value of the current state. 
+     * @param { action }  action  The details of the action.
+     */
+    dfRemove: (state, action) => {
+      
+      // Parse the action details
+      const id = action?.payload.id ?? null;
+
+      // Missing id
+      if(id == null)
+        return;
+      
+      // Invalid id
+      if(!state.dfs[id])
+        return;
+
+      // Delete entry
+      delete state.dfs[id];
+    },
+
+    /**
+     * Sets the state of the slice to 'loading'.
+     * 
+     * @param { state }   state   The value of the current state. 
+     * @param { action }  action  The details of the action.
+     */
+    dfSetStateLoading: (state, action) => {
+      state.state = 'loading';
+    },
+
+    /**
+     * Sets the state of the slice to 'loaded'.
+     * 
+     * @param { state }   state   The value of the current state. 
+     * @param { action }  action  The details of the action.
+     */
+    dfSetStateLoaded: (state, action) => {
+      state.state = 'loaded';
+    }
+  }
+})
+
+export const dfReducer = dfSlice.reducer;
+
+export default {
+  dfSlice,
+  dfReducer
+}
--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { dfReducer } from './df.slice'
 
+// Combine all the reducers of the different slices
+const rootReducer = combineReducers({
+  df: dfReducer
+})
+
+// Configure the store with the combined reducers
 export const store = configureStore({
-  reducer: {},
+  reducer: rootReducer,
 })
 
 export default {
