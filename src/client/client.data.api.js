9ba4a441052b7f3cb457ed80d139f75c90839345
Mo David <modavid.1964@gmail.com>
2024-06-28 21:46:02 +0800

    Added todo for tomorrow.

---
+++ src/client/client.data.api.js
@@ -0,0 +1,14 @@
+/**
+ * @ Author: Mo David
+ * @ Create Time: 2024-06-28 21:25:13
+ * @ Modified time: 2024-06-28 21:25:29
+ * @ Description:
+ * 
+ * Handles data management and interactions with redux.
+ */
+
+export const dataAPI = (function() {
+
+  const _ = {};
+
+})();
