465a28067f1a5d57834361e82092a54a119bc381
Mo David <modavid.1964@gmail.com>
2024-06-30 02:00:29 +0800

    Refactored toaster to make it more FP.

--- src/client/client.toast.js
+++ src/client/client.toast.js
@@ -1,7 +1,7 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-06-14 18:30:08
- * @ Modified time: 2024-06-14 20:23:49
+ * @ Modified time: 2024-06-30 01:58:02
  * @ Description:
  * 
  * This file deals with all the toasts the application might create.
@@ -17,41 +17,46 @@ export const ClientToast = (function() {
   const _loadingTitle = 'loading';
   const _failureTitle = 'failed';
 
-  /**
-   * Creates a new toast.
-   * 
-   * @param   { object }  options   The options to pass for the toast.
-   * @return  { string }            The id of the new toast.
-   */
-  _.createToast = function(toast, options={}) {
+  // The default style
+  const _style = {
     
-    // Grab the options
-    const successMessage = options.success ?? 'Request was processed.'
-    const loadingMessage = options.loading ?? 'Request is being processed.'
-    const failureMessage = options.failure ?? 'An error occurred.'
-    const promise = options.promise ?? new Promise(resolve => {
-      resolve();
-    })
+    // Styling
+    position: 'bottom-right',
+    variant: 'left-accent',
+    size: 'xs',
 
-    // The default style
-    const style = {
-      isClosable: true,
-      position: 'bottom-right',
-      duration: 2000,
-      variant: 'left-accent',
-      size: 'xs',
+    // Behavior
+    isClosable: true,
+    duration: 2000,
 
-      containerStyle: {
-        width: '512px',
-      },
-    }
+    // Container style
+    containerStyle: {
+      width: '512px',
+    },
+  }
 
-    // Create the toast
-    toast.promise(promise, {
-      success: { title: _successTitle, description: successMessage, ...style },
-      loading: { title: _loadingTitle, description: loadingMessage, ...style },
-      error: { title: _failureTitle, description: failureMessage, ...style },
-    });
+  /**
+   * Creates a function that creates a toast.
+   * 
+   * @param   { object }    options   The options for the toast. 
+   * @return  { function }            A function that creates the toast given the chakra toast object.
+   */
+  _.createToaster = function(options={}) {
+    return function(toast) {
+      
+      // Grab the options
+      const successMessage = options.success ?? 'Request was processed.'
+      const loadingMessage = options.loading ?? 'Request is being processed.'
+      const failureMessage = options.failure ?? 'An error occurred.'
+      const promise = options.promise ?? Promise.resolve('');
+
+      // Create the toast
+      toast.promise(promise, {
+        success: { title: _successTitle, description: successMessage, ..._style },
+        loading: { title: _loadingTitle, description: loadingMessage, ..._style },
+        error: { title: _failureTitle, description: failureMessage, ..._style },
+      });
+    } 
   }
 
   return {
