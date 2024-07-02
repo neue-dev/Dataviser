c8056e19a4a16a8aff0653d6f421fbd616b627f0
Mo David <modavid.1964@gmail.com>
2024-07-02 20:09:04 +0800

    Fixed useWindows hook.
    I didn't realize that simply returning a refernce to the window would
    not update the values _copied_ (the dimensions of the window) at the
    start of the component render (whichever component might have been
    using that hook).

---
+++ src/hooks/useWIndowDimensions.js
@@ -0,0 +1,47 @@
+/**
+ * @ Author: Mo David
+ * @ Create Time: 2024-07-01 23:18:42
+ * @ Modified time: 2024-07-02 20:07:36
+ * @ Description:
+ * 
+ * This was adapted from a stackoverflow post and lets us retrieve the window dimensions in react.
+ * For reference, see: https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs.
+ */
+
+import { useState, useEffect } from 'react';
+
+/**
+ * A hook that grabs the window information.
+ * 
+ * @hook
+ */
+export function useWindowDimensions() {
+
+  // Stores the window dimensions we were able to grab
+  const [ _width, _setWidth ] = useState(window.innerWidth);
+  const [ _height, _setHeight ] = useState(window.innerHeight);
+
+  // Allows us to handle dom effects
+  useEffect(() => {
+
+    // Update stored window sizes; window properties update on resize
+    const onResize = () => {
+      _setWidth(window.innerWidth);
+      _setHeight(window.innerHeight);
+    };
+
+    // Subscribe to resize events
+    window.addEventListener('resize', onResize);
+
+    // Return a callback to unsubscribe from resize events
+    return () => window.removeEventListener('resize', onResize);
+    
+  }, []);
+
+  // Return the window
+  return { width: _width, height: _height };
+}
+
+export default {
+  useWindowDimensions
+}
