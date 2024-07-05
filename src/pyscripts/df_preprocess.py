9820cdd65834b734a10cc55acfcd168879330160
Mo David <modavid.1964@gmail.com>
2024-07-06 06:49:45 +0800

    Created a sum df to get totals of all dfs.

--- src/pyscripts/df_preprocess.py
+++ src/pyscripts/df_preprocess.py
@@ -16,8 +16,13 @@ def preprocess(df):
   Preprocesses the df and makes sure it has the right columns and rows.
   '''
 
+  # Set the index and columns
   df = df.set_index(0)
   df.columns = df.iloc[0]
+  
+  # Grab all the numeric columns and convert them
+  cols = df.columns #.drop('')
+  df[cols] = df[cols].apply(pd.to_numeric, errors='coerce').fillna(0)
 
   return df
 
@@ -26,9 +31,20 @@ try:
 
   # For each piece of data in the dict, we save it
   for key in dfs:
+
+    # Create the dataframe and its meta data
     REF[key] = preprocess(pd.DataFrame(dfs[key]['df']))
     DFS[key] = REF[key]
     META[key] = dfs[key]['meta']
+
+    # If the sum df hasn't been defined yet
+    if 'sum' not in DFS:
+      META['sum'] = {}
+      DFS['sum'] = DFS[key]
+
+    # Get total of all dataframes too
+    else:
+      DFS['sum'] = DFS['sum'].add(DFS[key], fill_value=0).fillna(0)
     
   # Since we didn't modify anything, we just set OUT to dfs
   OUT = dfs
