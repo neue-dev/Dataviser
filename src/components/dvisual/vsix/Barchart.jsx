d419ebd471066374105605a627c42d5d0618fbe5
Mo David <modavid.1964@gmail.com>
2024-07-15 17:18:44 +0800

    Prettified barchart.

--- src/components/dvisual/vsix/Barchart.jsx
+++ src/components/dvisual/vsix/Barchart.jsx
@@ -1,7 +1,7 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-07-09 11:09:03
- * @ Modified time: 2024-07-15 16:08:56
+ * @ Modified time: 2024-07-15 17:08:51
  * @ Description:
  * 
  * Creates a bar chart using the visx library.
@@ -10,6 +10,7 @@
 import * as React from 'react'
 
 // Visx
+import { AnimatedAxis, AnimatedGridRows } from '@visx/react-spring'
 import { Group } from '@visx/group'
 import { Bar } from '@visx/shape'
 import { useTooltip, useTooltipInPortal, TooltipWithBounds } from '@visx/tooltip';
@@ -24,9 +25,10 @@ import * as Scale from '@visx/scale'
 export function Barchart(props={}) {
 
   // Grab the props
-  const _data = props.data;           // Contains all the data of all the series
-  const _width = props.width ?? 0;    // The width of the visual
-  const _height = props.height ?? 0;  // The height of the visual
+  const _data = props.data;             // Contains all the data of all the series
+  const _width = props.width ?? 0;      // The width of the visual
+  const _height = props.height ?? 0;    // The height of the visual
+  const _margin = props.margin ?? 50;   // The margins
   
   // Create the actual data we want
   const _chartData = []
@@ -85,7 +87,7 @@ export function Barchart(props={}) {
   })
 
   const yScale = Scale.scaleLinear({
-    range: [0, _height],
+    range: [_height - _margin, 0],
     round: true,
     domain: [0, 6000],  // ! dont hardcode this part
   })
@@ -107,15 +109,15 @@ export function Barchart(props={}) {
   };
   
   return (
-    <>
+    <div style={{ position: 'relative'}}>
       <svg ref={ _containerRef } width={ _width } height={ _height }>
         <Group>
           {_chartData.map((d) => {
 
             const barWidth = xScale.bandwidth();
-            const barHeight = yScale(d.y);
+            const barHeight = _height - _margin - yScale(d.y);
             const barX = xScale(d.x);
-            const barY = _height - barHeight;
+            const barY = _height - barHeight - _margin / 2;
 
             return (
               <Bar
@@ -125,12 +127,30 @@ export function Barchart(props={}) {
                 width={barWidth}
                 height={barHeight}
                 fill="rgba(23, 200, 217, 1)"
-                onMouseOver={ onMouseOver }
-                onMouseOut={ hideTooltip }
+                onMouseOver={ (e) => onMouseOver(e, d) }
+                onMouseOut={ (e) => hideTooltip(e) }
               />
             );
           })}
         </Group>
+        <AnimatedAxis orientation="bottom"
+          top={_height - _margin / 2 } 
+          scale={ xScale }
+          stroke={ 'rgba(0, 0, 0, 0.5)' }
+          tickLineProps={{
+            stroke: 'rgba(0, 0, 0, 0)'
+          }}
+        />
+        <AnimatedAxis orientation="left"
+          scale={ yScale }
+          top={ _margin / 2 } 
+          left={ _margin }
+          numTicks={ 4 }
+          stroke={ 'rgba(0, 0, 0, 0)' }
+          tickLineProps={{
+            stroke: 'rgba(0, 0, 0, 0.5)'
+          }}
+        />
       </svg>
       {tooltipOpen && (
         <_TooltipInPortal
@@ -139,9 +159,10 @@ export function Barchart(props={}) {
           top={tooltipTop}
           left={tooltipLeft}
         >
-          Data value <strong>{tooltipData}</strong>
+          <strong>{tooltipData.x}</strong><br />
+          {tooltipData.y}
         </_TooltipInPortal>
       )}
-    </>
+    </div>
   )
 }
